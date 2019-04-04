313188e7780b17b994acfa7c187feaffcb2b841c
Zheng ZHANG <zzcoolj@client-172-18-91-106.clients.u-psud.fr>
2019-04-04 17:36:23 +0200

    add gitignore; debug raw-txt-parser

--- raw-txt-parser.py
+++ raw-txt-parser.py
@@ -9,28 +9,48 @@ Target languages and files:
                     Chapter separator:      "Розділ "
                     Paragraph separator:    each line
     3.  Finnish     Finnish-content.txt (copy from HTML no images version)
+                    Chapter separator:      ends with "luku."
+                    Paragraph separator:    each line
+                    Special:                content ends at line 4628 (included)
 """
 
 
-def get_paragraph_info(file_path, para_separ, end_valid_line_num, file_total_lines):
+def get_paragraph_info(file_path, para_separ, para_separ_type, end_valid_line_num, file_total_lines):
+    """
+
+    :param file_path:
+    :param para_separ:
+    :param para_separ_type: 1:startswith 2:endswith
+    :param end_valid_line_num:
+    :param file_total_lines:
+    :return:
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.readlines()
     total_lines = len(data)
     if total_lines != file_total_lines:  # File information matching check
         print('[ERROR] #lines does not match.')
         print(total_lines)
-        print(data[2546])
         exit()
     chapter_count = 0
     chapter_content_starting_line = []
-    for i in range(total_lines-2):
-        if data[i].startswith(para_separ):
-            chapter_count += 1
-            chapter_content_starting_line.append(i+5)
+    if para_separ_type == 1:
+        for i in range(total_lines):
+            if data[i].startswith(para_separ):
+                chapter_count += 1
+                chapter_content_starting_line.append(i)
+    elif para_separ_type == 2:
+        for i in range(total_lines):
+            if data[i].endswith(para_separ):
+                chapter_count += 1
+                chapter_content_starting_line.append(i)
+    else:
+        print('[ERROR] para_separ_type invalid')
     if chapter_count != 43:
         print('[ERROR] #chapters is not 43.')
         print('#chapters: ', chapter_count)
         exit()
+    print(chapter_content_starting_line[:5])
 
     # We say each none-empty line in chapter is a paragraph EXCEPT some special content in the last chapter.
     paragraphs = []
@@ -58,5 +78,6 @@ def get_paragraph_info(file_path, para_separ, end_valid_line_num, file_total_lin
 
 
 get_paragraph_info('corpora/slavic/Bulgarian/Mark_Tven_-_Prikljuchenijata_na_Hykylberi_Fin_-1345-b.txt', '\tГлава ',
-                   2537, 2549-1)
-get_paragraph_info('corpora/slavic/Ukrainian/ukrainian-content.txt', 'Розділ ', 2423, 2423)
+                   1, 2537, 2549-1)
+get_paragraph_info('corpora/slavic/Ukrainian/ukrainian-content.txt', 'Розділ ', 1, 2423, 2423)
+get_paragraph_info('corpora/finno-ugric/Finnish/Finnish-content.txt', 'luku.\n', 2, 4628, 4768)
