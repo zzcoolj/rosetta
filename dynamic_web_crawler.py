2cdc1ac0da42284b8d478eb9c52e58bf6bf95a0e
Zheng ZHANG <zzcoolj@Zhengs-MacBook-Pro-2.local>
2019-05-11 23:13:17 -0700

    like a magic

--- dynamic_web_crawler.py
+++ dynamic_web_crawler.py
@@ -5,6 +5,15 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 driver.get('https://www.armiarma.eus/unibertsala/twain/')
-driver.switch_to.frame('bi')
-for element in driver.find_elements_by_xpath('//p'):
-    print(element.text)
+driver.switch_to.frame('bat')
+for link in driver.find_elements_by_xpath('//li/a'):
+    link.click()
+    driver.implicitly_wait(1)
+    driver.switch_to.default_content()
+    driver.switch_to.frame('bi')
+    for element in driver.find_elements_by_xpath('//p'):
+        print(element.text)
+    driver.switch_to.default_content()
+    driver.switch_to.frame('bat')
+
+driver.quit()
