b8ee7f84de4b6e5fbd9bd70823cf8a3c3ac0418d
Alex Zhai <zhaialex9@gmail.com>
2020-06-13 16:02:42 -0700

    js packages for map

---
+++ worldmap/addresspoints.js
@@ -0,0 +1,4 @@
+// index 1 is the status of the language: 0 = well, 1 = medium, 2 = under
+var addressPoints = [
+    ["Korean", 0, "Hok'ulperi p'in-ui mohom", "Kwak, Kyong-su", "1990", "Kumsong Youth Publishing House (Kumsong ch'ulp'ansa)", "Seoul, South Korea", 534, 37.5665, 126.9780],
+];
